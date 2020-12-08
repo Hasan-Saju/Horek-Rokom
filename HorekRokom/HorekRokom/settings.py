c4891abc88acb185b7d88c3a26fba03665418cd8
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2020-12-09 00:10:56 +0600

    setup done

--- HorekRokom/HorekRokom/settings.py
+++ HorekRokom/HorekRokom/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'App_Login',
+    'App_Shop',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
