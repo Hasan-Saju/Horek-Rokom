e764231e67a751b2afc09b2c7cfa585d10385ccd
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2020-12-23 15:28:48 +0600

    Messages and Authentication Complete

--- HorekRokom/App_Login/views.py
+++ HorekRokom/App_Login/views.py
@@ -10,6 +10,9 @@ from django.contrib.auth import login,logout,authenticate
 from App_Login.models import Profile
 from App_Login.forms import ProfileForm,SignUpForm
 
+# messages
+from django.contrib import messages
+
 
 # Create your views here.
 def sign_up(request):
@@ -18,6 +21,7 @@ def sign_up(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Account Created Successfully!")
             return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request,'App_Login/sign_up.htm',context={'form':form})
 
@@ -40,5 +44,21 @@ def login_user(request):
 @login_required
 def logout_user(request):
     logout(request)
+    messages.warning(request,"You are logged out!")
     return HttpResponse("Logged out")
+
+@login_required
+def user_profile(request):
+    profile=Profile.objects.get(user=request.user)
+
+    form = ProfileForm(instance=profile)
+    if request.method=='POST':
+        form=ProfileForm(request.POST,instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Profile Updated Successfully!")
+            form=ProfileForm(instance=profile)
+    return render(request,'App_LOgin/change_profile.htm',context={'form':form})
+
+
 
