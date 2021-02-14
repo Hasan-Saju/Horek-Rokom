13c7261a60266f3a6180bce7feea6999093062cb
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-02-14 16:52:11 +0600

    category wise shopping

--- HorekRokom/App_Shop/urls.py
+++ HorekRokom/App_Shop/urls.py
@@ -7,4 +7,6 @@ app_name = 'App_Shop'
 urlpatterns = [
     path('',views.Home.as_view(),name='home'),
     path('product_details/<pk>/',views.ProductDetail.as_view(),name='product_details'),
+    path('category/',views.View_Category,name='category'),
+    path('categoryWise/<pk>/',views.ProductDetail_from_Category,name='categoryWise'),
 ]
--- HorekRokom/App_Shop/views.py
+++ HorekRokom/App_Shop/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 #Models
-from App_Shop.models import Product
+from App_Shop.models import Product,Category
 
 # Mixin
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -21,4 +21,25 @@ class ProductDetail(LoginRequiredMixin, DetailView):
     model=Product
     context_object_name="product"
     template_name='App_Shop/product_details.htm'
+
+def View_Category(request):
+    # model=Category
+    # context_object_name="category"
+    # template_name='App_Shop/category.htm'
+    category=Category.objects.all()
+    print(category)
+    print("hi")
+    dict={"category":category}
+    return render(request,"App_Shop/category.htm",context=dict)
+
+def ProductDetail_from_Category(request,pk):
+    # print("hello")
+    products=Product.objects.filter(category=pk)
+    # print("hi")
+    # print(products)
+    categoryName=Category.objects.get(pk=pk)
+    # print()
+    dict={"object_list":products,"category":"fromCategory","name":str(categoryName)}
+    return render(request,"App_Shop/home.htm",context=dict)
+
     
